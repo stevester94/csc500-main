4578e8a07fe30141c26076d0082489949815a113
Steven Mackey <stevester94@gmail.com>
2022-01-31 19:57:54 -0800

    Making progress on notebooks

--- source_this_after_venv.sh
+++ source_this_after_venv.sh
@@ -3,3 +3,4 @@ export PYTHONPATH=$(realpath csc500-utils):$PYTHONPATH
 export PYTHONPATH=$(realpath csc500-models):$PYTHONPATH
 export DATASETS_ROOT_PATH=$PWD/datasets/
 export CSC500_ROOT_PATH=$PWD
+export CUBLAS_WORKSPACE_CONFIG=:4096:8
