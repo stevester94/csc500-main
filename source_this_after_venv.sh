d146730957957028116a06d9c816267eeb123be7
Steven Mackey <stevester94@gmail.com>
2022-03-03 08:24:34 -0800

    Add experiment var

--- source_this_after_venv.sh
+++ source_this_after_venv.sh
@@ -2,5 +2,6 @@ ulimit -n $(ulimit -Hn)
 export PYTHONPATH=$(realpath csc500-utils):$PYTHONPATH
 export PYTHONPATH=$(realpath csc500-models):$PYTHONPATH
 export DATASETS_ROOT_PATH=$PWD/datasets/
+export EXPERIMENTS_ROOT_PATH=$PWD/csc500-notebooks/experiments/
 export CSC500_ROOT_PATH=$PWD
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
