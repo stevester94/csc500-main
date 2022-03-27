3d6a87925ba024089aaa3eb643ea23f224b99419
Steven Mackey <stevester94@gmail.com>
2022-03-27 13:43:25 -0700

    Begin final analysis using unified pandas approach. Still need some experiments (cnn, tl_3v2, tuned_1v2)

--- source_this_after_venv.sh
+++ source_this_after_venv.sh
@@ -1,6 +1,8 @@
 ulimit -n $(ulimit -Hn)
 export PYTHONPATH=$(realpath csc500-utils):$PYTHONPATH
 export PYTHONPATH=$(realpath csc500-models):$PYTHONPATH
+export PYTHONPATH=$(realpath csc500-notebooks/utils/):$PYTHONPATH
+
 export DATASETS_ROOT_PATH=$PWD/datasets/
 export EXPERIMENTS_ROOT_PATH=$PWD/csc500-notebooks/experiments/
 export CSC500_ROOT_PATH=$PWD
